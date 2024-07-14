633669c6618095688dde1d5200151436a3f07c41
John Chen <149972379+jche1156@users.noreply.github.com>
2024-07-14 12:10:13 -0700

    Create early commands

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # bibot
 Web API for eventual Discord chatbot
+
+https://python.langchain.com/v0.2/docs/langserve/
---
+++ bibot/src/main.rs
@@ -0,0 +1,98 @@
+use anyhow::Context as _;
+use anyhow::Result;
+use notion::NotionApi;
+use poise::serenity_prelude::CacheHttp;
+use poise::serenity_prelude::{ClientBuilder, GatewayIntents};
+use rand::Rng;
+use serde::{Deserialize, Serialize};
+use shuttle_runtime::SecretStore;
+use shuttle_serenity::ShuttleSerenity;
+
+struct Data {} // User data, which is stored and accessible in all command invocations
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
+
+/// Responds with a greeting
+#[poise::command(slash_command)]
+async fn hello(ctx: Context<'_>) -> Result<(), Error> {
+    ctx.say(", CCSF CS Club!").await?;
+    Ok(())
+}
+
+/// Rolls a D6
+#[poise::command(slash_command)]
+async fn dice(ctx: Context<'_>) -> Result<(), Error> {
+    let num = rand::thread_rng().gen_range(1..7);
+    ctx.say(format!("You number is: {}", num)).await?;
+    Ok(())
+}
+
+#[derive(Serialize, Deserialize)]
+struct Report {
+    title: String,
+    summary: String,
+    news_site: String,
+    url: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Reports {
+    results: Vec<Report>,
+}
+
+/// Reports on the ISS' latest shenanigans
+#[poise::command(slash_command)]
+async fn iss(ctx: Context<'_>) -> Result<(), Error> {
+    let body = reqwest::get("https://api.spaceflightnewsapi.net/v4/reports/?format=json&limit=1")
+        .await?
+        .text()
+        .await?;
+    let r: Reports = serde_json::from_str(&body)?;
+    ctx.say(format!(
+        "{}: {}\n{}\n\n[Read More]({})",
+        r.results[0].news_site, r.results[0].title, r.results[0].summary, r.results[0].url
+    ))
+    .await?;
+    Ok(())
+}
+
+// Lists upcoming events for the week
+#[poise::command(slash_command)]
+async fn this_week(ctx: Context<'_>) -> Result<(), Error> {
+    // let client = NotionApi::new();
+
+    let body = "Nothing Yet";
+    ctx.say(format!(
+        "Here are the upcoming events for the week in our calendar:\n{}",
+        body
+    ))
+    .await?;
+    Ok(())
+}
+
+#[shuttle_runtime::main]
+async fn main(#[shuttle_runtime::Secrets] secret_store: SecretStore) -> ShuttleSerenity {
+    // Get the discord token set in `Secrets.toml`
+    let discord_token = secret_store
+        .get("DISCORD_TOKEN")
+        .context("'DISCORD_TOKEN' was not found")?;
+
+    let framework = poise::Framework::builder()
+        .options(poise::FrameworkOptions {
+            commands: vec![hello(), dice(), iss(), this_week()],
+            ..Default::default()
+        })
+        .setup(|ctx, _ready, framework| {
+            Box::pin(async move {
+                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                Ok(Data {})
+            })
+        })
+        .build();
+
+    let client = ClientBuilder::new(discord_token, GatewayIntents::non_privileged())
+        .framework(framework)
+        .await
+        .map_err(shuttle_runtime::CustomError::new)?;
+    Ok(client.into())
+}
