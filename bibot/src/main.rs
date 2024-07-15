5cd86b0e72016716d4ea0e0ef03bc6ad1436a281
John Chen <149972379+jche1156@users.noreply.github.com>
2024-07-14 19:38:33 -0700

    Add vendored rust Notion API and MVP notion API chat command

--- bibot/src/main.rs
+++ bibot/src/main.rs
@@ -1,14 +1,21 @@
 use anyhow::Context as _;
 use anyhow::Result;
-use notion::NotionApi;
-use poise::serenity_prelude::CacheHttp;
 use poise::serenity_prelude::{ClientBuilder, GatewayIntents};
 use rand::Rng;
+use rusticnotion::ids::BlockId;
+use rusticnotion::ids::DatabaseId;
+use rusticnotion::models::block::Block;
+use rusticnotion::models::search::DatabaseQuery;
+use rusticnotion::models::Database;
+use rusticnotion::NotionApi;
 use serde::{Deserialize, Serialize};
 use shuttle_runtime::SecretStore;
 use shuttle_serenity::ShuttleSerenity;
+use std::str::FromStr;
 
-struct Data {} // User data, which is stored and accessible in all command invocations
+struct Data {
+    notion_api_key: String,
+} // User data, which is stored and accessible in all command invocations
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
 
@@ -59,14 +66,44 @@ async fn iss(ctx: Context<'_>) -> Result<(), Error> {
 // Lists upcoming events for the week
 #[poise::command(slash_command)]
 async fn this_week(ctx: Context<'_>) -> Result<(), Error> {
-    // let client = NotionApi::new();
+    let client: NotionApi = NotionApi::new(ctx.data().notion_api_key.clone())?;
+    let db_id: DatabaseId = DatabaseId::from_str("bc689835f6904f83871cb67efe6b7d1e")?;
+    let db: Database = client.get_database(db_id).await?;
+    let query: DatabaseQuery = DatabaseQuery {
+        sorts: None,
+        filter: None,
+        paging: None,
+    };
+    let response = client.query_database(db, query).await?;
+    let page = response.results.get(0).expect("No events");
+    let mut body = String::from("");
+    // Title
+    body.push_str(format!("**{}**\n", page.title().ok_or("Untitled")?).as_str());
 
-    let body = "Nothing Yet";
+    // Body Text
+    let children = client
+        .get_block_children(BlockId::from(page.id.clone()))
+        .await?;
+    for child in children.results {
+        if let Block::Paragraph { paragraph, .. } = child {
+            for t in paragraph.rich_text {
+                body.push_str(t.plain_text())
+            }
+        }
+    }
+
     ctx.say(format!(
         "Here are the upcoming events for the week in our calendar:\n{}",
         body
     ))
     .await?;
+    Ok(())
+}
+
+// Makes it partytime for 1 hour
+#[poise::command(slash_command)]
+async fn partytime(ctx: Context<'_>) -> Result<(), Error> {
+    ctx.say("/create title:dev_event datetime:in 5 seconds description:test dev event duration:15 seconds channel:#bot-spam").await?;
     Ok(())
 }
 
@@ -76,16 +113,21 @@ async fn main(#[shuttle_runtime::Secrets] secret_store: SecretStore) -> ShuttleS
     let discord_token = secret_store
         .get("DISCORD_TOKEN")
         .context("'DISCORD_TOKEN' was not found")?;
+    let notion_token = secret_store
+        .get("NOTION_API_KEY")
+        .context("'NOTION_API_KEY' was not found");
 
     let framework = poise::Framework::builder()
         .options(poise::FrameworkOptions {
-            commands: vec![hello(), dice(), iss(), this_week()],
+            commands: vec![hello(), dice(), iss(), this_week(), partytime()],
             ..Default::default()
         })
         .setup(|ctx, _ready, framework| {
             Box::pin(async move {
                 poise::builtins::register_globally(ctx, &framework.options().commands).await?;
-                Ok(Data {})
+                Ok(Data {
+                    notion_api_key: notion_token.expect("Error finding Notion API Key"),
+                })
             })
         })
         .build();
---
+++ bibot/vendor/rusticnotion/src/models/users.rs
@@ -0,0 +1,39 @@
+use crate::ids::UserId;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone, Debug, Eq, PartialEq)]
+pub struct UserCommon {
+    pub id: UserId,
+    pub name: Option<String>,
+    pub avatar_url: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, Eq, PartialEq)]
+pub struct Person {
+    pub email: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, Eq, PartialEq)]
+pub struct Bot {
+    pub email: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, Eq, PartialEq)]
+#[serde(tag = "object", rename_all = "snake_case")]
+pub enum User {
+    Person {
+        #[serde(flatten)]
+        common: UserCommon,
+        person: Person,
+    },
+    Bot {
+        #[serde(flatten)]
+        common: UserCommon,
+        bot: Bot,
+    },
+    User {
+        #[serde(flatten)]
+        common: UserCommon,
+        person: Option<Person>,
+    },
+}
